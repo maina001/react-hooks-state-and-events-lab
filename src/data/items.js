e8e58a3a4d32b3b95394ded9e9c704dab2085afb
Brian <brianmakorious@gmail.com>
2023-01-18 15:35:08 +0300

    done

--- src/data/items.js
+++ src/data/items.js
@@ -1,9 +1,26 @@
-const items = [
-  { id: 1, name: "Yogurt", category: "Dairy" },
-  { id: 2, name: "Pomegranate", category: "Produce" },
-  { id: 3, name: "Lettuce", category: "Produce" },
-  { id: 4, name: "String Cheese", category: "Dairy" },
-  { id: 5, name: "Cookies", category: "Dessert" },
-];
+import React, { useState } from "react";
 
-export default items;
+function Item({ name, category }) {
+// use state to keep track of whether the item is in the cart or not
+const [inCart, setInCart] = useState(false);
+
+function handleClick() {
+// toggle the inCart state
+setInCart(!inCart);
+}
+
+const itemClass = inCart ? "in-cart" : "";
+const buttonText = inCart ? "Remove From Cart" : "Add to Cart";
+
+return (
+<li className={itemClass}>
+<span>{name}</span>
+<span className="category">{category}</span>
+<button className="add" onClick={handleClick} >
+{buttonText}
+</button>
+</li>
+);
+}
+
+export default Item;
